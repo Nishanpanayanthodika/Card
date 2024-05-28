e7b03627639d84caa47e7854782725cf7e825d69
Nishanpanayanthodika <panayant@sheridancollege.ca>
2024-05-28 11:25:58 -0400

    Update CardTrick.java
    Lucky Card Added

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -55,6 +55,24 @@ public class CardTrick {
         } else {
             System.out.println("Sorry, your card is not in the magic hand.");
         }
+        
+        Card luckyCard = new Card();
+        luckyCard.setValue(2);
+        luckyCard.setSuit("Clubs");
+
+        boolean luckyFound = false;
+        for (Card card : magicHand) {
+            if (card.getValue() == luckyCard.getValue() && card.getSuit().equals(luckyCard.getSuit())) {
+                luckyFound = true;
+                break;
+            }
+        }
+
+        if (luckyFound) {
+            System.out.println("Congratulations! The lucky card is in the magic hand.");
+        } else {
+            System.out.println("Sorry, the lucky card is not in the magic hand.");
+        }
 
        
     }
