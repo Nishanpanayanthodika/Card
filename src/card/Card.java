4a11d756210047a917cc87e7927cdc7808214d43
Nishanpanayanthodika <panayant@sheridancollege.ca>
2024-05-28 04:01:53 -0400

    version 1 of my Card project

--- src/card/Card.java
+++ src/card/Card.java
@@ -12,6 +12,8 @@ package card;
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
  * @author srinivsi
+ * @modifier Nishan Panayanthodika
+ * Student number: 991728690
  */
 public class Card {
 
